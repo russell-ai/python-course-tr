a115610ac2c96eee93e31338b4f5288c9de60180
resulcaliskan <resulcaliskan@yahoo.com>
2020-04-05 00:42:43 +0300

    yeni biten güncellemer eklendi.

---
+++ Bölüm 1 Giriş/tanışma.py
@@ -0,0 +1,10 @@
+# İlk örnek uygulama programı, 
+# input ve print fonksiyonları ile hazırlanmıştır.
+
+print("Herkes için python kursuna hoş geldin")  # ekrana yazdırır.
+isim = input("lütfen isminizi giriniz :")       # kullanıcıdan isim değişkeni için değer girilmesini ister.
+print("Merhaba ",isim)
+yaş= input("Kaç yaşındasın :")                  # kullanıcı yaşını yaş değişkenine atar.
+doğumyılı = 2020-int(yaş)                       # doğum yılını hesaplar.
+print("Tahminime göre doğum yılın:",doğumyılı)
+print("Tekrar görüşmek üzere hoşcakalın")
